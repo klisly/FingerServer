8084d65b9d1021140f301715b80ed30f468cce93
wizardholy <wizardholy@163.com>
2016-11-26 23:18:10 +0800

    抓去策略优化

--- utils/crawlutil.js
+++ utils/crawlutil.js
@@ -23,12 +23,10 @@ function search(name, callback) {
             q: name
         },
         headers: {
-            'host':"117.42.202.193",
-            'port':"808",
             'postman-token': '139bd025-2543-1f5d-bd86-08dd9d67f735',
             'cache-control': 'no-cache',
             "gzip": "true",
-            "User-Agent":"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; Trident/5.0; SLCC2;.NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0"
+            "User-Agent": "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; Trident/5.0; SLCC2;.NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0"
         }
     };
 
@@ -74,7 +72,7 @@ function crawUpdates(novel, callback) {
             var count = 0;
             var maxCount = 10;
             var stop = false;
-            if(datas.length <= 0){
+            if (datas.length <= 0) {
 
                 return;
             }
@@ -111,42 +109,46 @@ function crawUpdates(novel, callback) {
                         chapter.createAt = new Date().getTime();
                         chapter.updateAt = chapter.createAt;
                         if (chapter.content.length > 500) {
-                                Chapter.update({"no": chapter.no,"nid": chapter.nid}, chapter, {upsert: true})
+                            var updateInfo = {
+                                "latest": chapter.title,
+                                "latestno": chapter.no,
+                                "updateAt": new Date().getTime(),
+                            }
+                            Chapter.update({"no": chapter.no, "nid": chapter.nid}, chapter, {upsert: true})
                                 .exec()
-                                .then((data)=>{
-                                    console.log("crawl chapter:"+JSON.stringify(chapter)+" res:"+JSON.stringify(data));
-                                    return new Promise((resolve, reject)=>{
-                                        Novel.update({"_id": novel._id.toString(), "no": {"$lt": parseInt(chapter.no)}}, {
-                                            "latest": chapter.title,
-                                            "latestno": chapter.no,
-                                            "updateAt": new Date().getTime(),
-                                        }).exec(function (err, resData) {
-                                            if(err){
-                                                reject(err)
-                                            } else {
-                                                resolve(resData)
-                                            }
-                                        })
+                                .then((data)=> {
+                                    console.log("crawl chapter:" + JSON.stringify(chapter) + " res:" + JSON.stringify(data));
+                                    return new Promise((resolve, reject)=> {
+                                        Novel.update({"_id": novel._id.toString(), "no": {"$lt": chapter.no}}, {"$set":updateInfo})
+                                            .exec(function (err, resData) {
+                                                if (err) {
+                                                    reject(err)
+                                                } else {
+                                                    resolve(resData)
+                                                }
+                                            })
                                     })
                                 })
-                                .then((data)=>{
-                                    console.log("novel udpate:"+JSON.stringify(data));
-                                    return new Promise((resolve, reject)=>{
-                                        User2Novel.update({"nid": novel._id.toString(), "latestno": {"$lt": parseInt(chapter.no)}}, {
-                                            "latestno": chapter.no,
-                                            "latest": chapter.title,
-                                            "updateAt": new Date().getTime(),
-                                        }).exec(function (err, resData) {
-                                            if(err){
-                                                reject(err)
-                                            } else {
-                                                resolve(resData)
-                                            }
-                                        })
+                                .then((data)=> {
+                                    console.log("novel udpate:" + JSON.stringify(data));
+                                    return new Promise((resolve, reject)=> {
+                                        User2Novel.update({
+                                            "nid": novel._id.toString(),
+                                            "latestno": {"$lt": chapter.no}
+                                        }, {"$set":updateInfo})
+                                            .exec(function (err, resData) {
+                                                if (err) {
+                                                    reject(err)
+                                                } else {
+                                                    resolve(resData)
+                                                }
+                                            })
                                     })
-                                })
-                                .catch((err)=>{
-                                    console.log("err:"+err.message);
+                                }).then((data)=> {
+                                console.log("user2novel udpate:" + JSON.stringify(data));
+                            })
+                                .catch((err)=> {
+                                    console.log("err:" + err.message);
                                 })
                         }
                     } catch (e) {
@@ -215,26 +217,26 @@ function crawlPage(url, callback) {
         var index = util.getRandomNum(0, config.ips.length);
         let host;
         let port;
-        if(config.ips.length > 0 ){
+        if (config.ips.length > 0) {
             host = config.ips[index % config.ips.length][0];
             port = parseInt(config.ips[index % config.ips.length][1]);
         }
-        var proxyUri = 'http://'+host+':'+port;
+        var proxyUri = 'http://' + host + ':' + port;
         var options = {
             method: 'GET',
             url: url,
-            proxy:proxyUri,
+            proxy: proxyUri,
             headers: {
                 'postman-token': '139bd025-2543-1f5d-bd86-08dd9d67f735',
                 'cache-control': 'no-cache',
                 "gzip": "true",
-                "User-Agent":"Mozilla/5.0 (Linux; Android 5.1.1; Nexus 6 Build/LYZ28E) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.23 Mobile Safari/537.36"
+                "User-Agent": "Mozilla/5.0 (Linux; Android 5.1.1; Nexus 6 Build/LYZ28E) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.23 Mobile Safari/537.36"
             },
         };
-        if( parseInt(count["count_" + url])  == maxtry || host == undefined){
+        if (parseInt(count["count_" + url]) == maxtry || host == undefined) {
             delete options["proxy"];
         }
-        console.log("options:"+JSON.stringify(options));
+        console.log("options:" + JSON.stringify(options));
         count["count_" + url] = count["count_" + url] + 1;
         if (parseInt(count[" " + url]) > maxtry) {
             return;
@@ -257,15 +259,15 @@ function crawProxy() {
     crawlPage("http://api.xicidaili.com/free2016.txt", function (err, body) {
         var ips = body.split("\r\n");
         var ipArray = []
-        for(var index = 0; index < ips.length; index++){
+        for (var index = 0; index < ips.length; index++) {
             ipArray.push(ips[index].split(":"))
         }
         config.ips = ipArray;
-        console.log("ipArray:"+config.ips);
+        console.log("ipArray:" + config.ips);
     })
 }
 module.exports = {
     search: search,
     crawUpdates: crawUpdates,
-    crawlProxy:crawProxy
+    crawlProxy: crawProxy
 }
