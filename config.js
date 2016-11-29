2d175118eed70c46c2067917e136baebd29e4627
wizardholy <wizardholy@163.com>
2016-11-29 12:38:42 +0800

    3分钟 检查通知一次

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   cronmag:'1 */15 8,19 * * *',  // 杂志生成
   croncrawl:'1 */10 0-1,10-23 * * *', // 10分钟抓取一次
   cronproxy:'1 */15 0-1,10-23 * * *', // 10分钟抓取一次
-  cronnotifi:'1 */5 8-23 * * *', // 5分钟通知一次一次
+  cronnotifi:'1 */3 * * * *', // 3分钟通知一次一次
   max_page:3,
   pass:"3cb028cf2810",
   umeng_master_key:"xh0hpg7adsbhvgttignixvugsen0rfgx", //友盟的推送key
